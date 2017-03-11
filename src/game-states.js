358f711722b4a16b5db2a5236b2df9208d97c566
ovidiubute <ovidiu.bute@gmail.com>
2017-03-11 16:01:49 +0200

    Remove default export

--- src/game-states.js
+++ src/game-states.js
@@ -1,9 +1,3 @@
 export const GAME_OVER = Symbol('OVER');
 export const GAME_STARTED = Symbol('BEGIN');
 export const GAME_WON = Symbol('WIN');
-
-export default {
-  GAME_WON,
-  GAME_OVER,
-  GAME_STARTED,
-};
